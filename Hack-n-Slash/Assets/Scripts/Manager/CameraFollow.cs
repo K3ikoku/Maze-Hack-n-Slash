de9db7eaaf1bd8b66b698690b9de02fb9f96f69a
Gustaf Wall <wallgu@utb.fuv.se>
2016-02-24 13:13:33 +0100

    Added the morph3d files and created a camerafollow script

---
+++ Hack-n-Slash/Assets/Scripts/Manager/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform mTarget;
+    [SerializeField] private float mSmoothing = 5;
+
+    Vector3 mOffset;
+
+    void Start()
+    {
+        //Set the offset of the camera to the player
+        mOffset = transform.position - mTarget.position;
+    }
+
+    void FixedUpdate()
+    {
+        //Make the camera follow the player and smooth it with a lerp
+        Vector3 mTargetCamPos = mTarget.position + mOffset;
+        transform.position = Vector3.Lerp(transform.position, mTargetCamPos, mSmoothing * Time.deltaTime);
+    }
+}
