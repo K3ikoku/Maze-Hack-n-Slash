2cd28124d28a62daa0aa07cc454c717b8d9bcdba
unknown <karlse@3se-spel-22.utb.fuv.se>
2016-02-29 16:13:15 +0100

    added some empty scripts in player folder added an enemy health script in enemys and added a new folder for bullets & projectiles!

---
+++ Hack-n-Slash/Assets/Scripts/Bullets & Projectiles/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnCollisionEnter (Collision other)
+    {
+       if (other.transform.tag == "Enemy")
+        {
+            EnemyBehaviour enemy = other.transform.GetComponent<EnemyBehaviour>();
+            enemy.EnemyHealth -= 50;
+            DestroyObject(gameObject);
+        }
+            
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Hack-n-Slash/Assets/Scripts/Enemies/Enemy Behaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+    private int mEnemyHealth = 100;
+    
+
+    public int EnemyHealth
+    {
+        get { return mEnemyHealth; }
+
+        set { mEnemyHealth = value; }       
+    }
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
