6ab567d3993072c7ffbb42afcdcadec5e7ac5e58
unknown <nygrjo@3se-spel-15.utb.fuv.se>
2016-03-02 16:46:56 +0100

    Added script for ai

---
+++ Hack-n-Slash/Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float playerDist;
+    public float enemyLookDist;
+    public float attackDist;
+    public float enemyMoveSpeed;
+    public float damping;
+    public Transform playerTarget;
+    Rigidbody theRidgidBody;
+    Renderer myRender;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRender = GetComponent<Renderer>();
+        theRidgidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerDist = Vector3.Distance(playerTarget.position, transform.position);
+
+        if(playerDist < enemyLookDist)
+        {
+            myRender.material.color = Color.yellow;
+            lookAtPlayer();
+        }
+
+        if(playerDist < attackDist)
+        {
+            myRender.material.color = Color.red;
+            attackPlayer();
+            print("Attck!");
+        }
+        else
+        {
+            myRender.material.color = Color.blue;
+        }
+    }
+
+    void lookAtPlayer()
+    {
+        Quaternion rotation = Quaternion.LookRotation(playerTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+    }
+
+    void attackPlayer()
+    {
+        theRidgidBody.AddForce(transform.forward * enemyMoveSpeed);
+    }
+}
