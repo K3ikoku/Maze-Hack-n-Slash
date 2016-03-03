d2e0ddd0652f56794591022a30e030b034ee7344
unknown <nygrjo@3se-spel-15.utb.fuv.se>
2016-03-03 15:26:12 +0100

    Fixad enemy movement when close

---
+++ Hack-n-Slash/Assets/Scripts/Enemies/AItest2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AItest2 : MonoBehaviour
+{
+
+
+   //[SerializeField] private float mSpeed;
+   [SerializeField] private Transform mTarget;
+   [SerializeField] private int mMinRange;
+   [SerializeField] private bool mFollow;
+ 
+ void Update()
+    {
+        //float step = mSpeed * Time.deltaTime;
+        //transform.position = Vector3.MoveTowards(transform.position, mTarget.position, step);
+        if (Vector3.Distance(transform.position, mTarget.position) < mMinRange)
+            mFollow = true;
+        if (mFollow)
+        {
+            transform.LookAt(mTarget);
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+}
+
