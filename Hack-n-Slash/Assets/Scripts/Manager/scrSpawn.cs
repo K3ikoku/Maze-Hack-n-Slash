c33a576816ee92b3fe29820f82f137c293bc8561
Oskar Fahlgren <fahlos@utb.fuv.se>
2016-03-10 15:30:18 +0100

    Level Prefabs and prefabspawner

---
+++ Hack-n-Slash/Assets/Scripts/Manager/scrSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrSpawn : MonoBehaviour {
+
+    // Oskar Fahlgren
+    // Random spawn of prefabs
+
+    [SerializeField] private Transform[] mTeleport;
+    [SerializeField] private GameObject[] mPrefeb;
+
+
+    // Use this for initialization
+    void Start () {
+        int tele_num = Random.Range(0, 1);
+        int prefeb_num = Random.Range(0, 3);
+
+     
+
+        Instantiate (mPrefeb[prefeb_num], mTeleport[tele_num].position, mTeleport[tele_num].rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
