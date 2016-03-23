974e408759c5abd5bad694a5d086a96bad52b252
unknown <nygrjo@3se-spel-15.utb.fuv.se>
2016-03-23 14:32:40 +0100

    kill count som nästan funkar, collision på träd, bullet effect, o kanske nåt mer. Glänns fel om allt pajar nu

---
+++ Hack-n-Slash/Assets/Win_condition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win_condition : MonoBehaviour
+{
+    [SerializeField] private GameObject mPortal;
+    private int mNrOfEnemies;
+
+    public int EnemiesLeft
+    {
+        get { return mNrOfEnemies; }
+        set { mNrOfEnemies = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnemiesLeft >= 0)
+
+        {
+            mPortal.SetActive(true);
+        }
+    }
+
+
+    void OnGUI()
+    {
+        {
+            GUI.Label(new Rect(0, 0, 200, 20), "Enemies Remaining : " + EnemiesLeft);
+        }
+    }
+}
